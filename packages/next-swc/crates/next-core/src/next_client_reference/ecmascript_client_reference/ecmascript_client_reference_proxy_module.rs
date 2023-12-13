44322314d85300de60c0d61a5c21c11e25c54df5
hrmny <8845940+ForsakenHarmony@users.noreply.github.com>
2023-12-13 15:35:29 +0100

    feat(turbopack-ecmascript): support named client references

--- packages/next-swc/crates/next-core/src/next_client_reference/ecmascript_client_reference/ecmascript_client_reference_proxy_module.rs
+++ packages/next-swc/crates/next-core/src/next_client_reference/ecmascript_client_reference/ecmascript_client_reference_proxy_module.rs
@@ -1,6 +1,7 @@
 use std::{io::Write, iter::once};
 
 use anyhow::{bail, Context, Result};
+use indexmap::IndexSet;
 use indoc::writedoc;
 use turbo_tasks::{Value, ValueToString, Vc};
 use turbo_tasks_fs::File;
@@ -65,41 +66,105 @@ impl EcmascriptClientReferenceProxyModule {
     }
 
     #[turbo_tasks::function]
-    async fn proxy_module(self: Vc<Self>) -> Result<Vc<EcmascriptModuleAsset>> {
-        let this = self.await?;
+    async fn proxy_module(&self) -> Result<Vc<EcmascriptModuleAsset>> {
         let mut code = CodeBuilder::default();
+
+        let server_module_path = &*self.server_module_ident.path().to_string().await?;
 
         // Adapted from
         // next.js/packages/next/src/build/webpack/loaders/next-flight-loader/index.ts
-        writedoc!(
-            code,
-            r#"
-                import {{ createProxy }} from 'next/dist/build/webpack/loaders/next-flight-loader/module-proxy'
+        if let EcmascriptExports::EsmExports(exports) = &*self.client_module.get_exports().await? {
+            let mut exported_names = exports
+                .await?
+                .exports
+                .keys()
+                .cloned()
+                .collect::<IndexSet<_>>();
 
-                const proxy = createProxy({server_module_path})
+            for result in exports.expand_star_exports().await? {
+                let export_info = result.await?;
 
-                // Accessing the __esModule property and exporting $$typeof are required here.
-                // The __esModule getter forces the proxy target to create the default export
-                // and the $$typeof value is for rendering logic to determine if the module
-                // is a client boundary.
-                const {{ __esModule, $$typeof }} = proxy;
+                exported_names.extend(export_info.star_exports.iter().cloned());
 
-                export {{ __esModule, $$typeof }};
-                export default proxy;
-            "#,
-            server_module_path = StringifyJs(&this.server_module_ident.path().to_string().await?)
-        )?;
+                if export_info.has_dynamic_exports {
+                    // TODO: throw? warn?
+                }
+            }
+
+            writedoc!(
+                code,
+                r#"
+                    import {{ createProxy }} from 'next/dist/build/webpack/loaders/next-flight-loader/module-proxy';
+
+                    const proxy = createProxy({server_module_path});
+
+                    // Accessing the __esModule property and exporting $$typeof are required here.
+                    // The __esModule getter forces the proxy target to create the default export
+                    // and the $$typeof value is for rendering logic to determine if the module
+                    // is a client boundary.
+                    const {{ __esModule, $$typeof }} = proxy;
+                    const __default__ = proxy.default;
+                "#,
+                server_module_path = StringifyJs(server_module_path),
+            )?;
+
+            let mut cnt: i32 = 0;
+
+            for client_ref in exported_names {
+                if client_ref.is_empty() {
+                    // not sure when this would occur, copied it from the next.js version
+                    writedoc!(
+                        code,
+                        r#"
+                            exports[''] = createProxy({server_module_path});
+                        "#,
+                        server_module_path = StringifyJs(&format!("{}#", server_module_path)),
+                    )?;
+                } else if client_ref == "default" {
+                    writedoc!(
+                        code,
+                        r#"
+                            export {{ __esModule, $$typeof }};
+                            export default __default__;
+                        "#,
+                    )?;
+                } else {
+                    writedoc!(
+                        code,
+                        r#"
+                            const e{cnt} = createProxy({server_module_path});
+                            export {{ e{cnt} as {client_ref} }};
+                        "#,
+                        server_module_path =
+                            StringifyJs(&format!("{}#{}", server_module_path, client_ref)),
+                        cnt = cnt,
+                        client_ref = client_ref,
+                    )?;
+                    cnt += 1;
+                }
+            }
+        } else {
+            writedoc!(
+                code,
+                r#"
+                    const {{ createProxy }} = require('next/dist/build/webpack/loaders/next-flight-loader/module-proxy');
+
+                    module.exports = createProxy({server_module_path});
+                "#,
+                server_module_path = StringifyJs(server_module_path)
+            )?;
+        };
 
         let code = code.build();
         let proxy_module_content =
             AssetContent::file(File::from(code.source_code().clone()).into());
 
         let proxy_source = VirtualSource::new(
-            this.server_module_ident.path().join("proxy.ts".to_string()),
+            self.server_module_ident.path().join("proxy.ts".to_string()),
             proxy_module_content,
         );
 
-        let proxy_module = this
+        let proxy_module = self
             .server_asset_context
             .process(
                 Vc::upcast(proxy_source),
